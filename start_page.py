4658192d2d7d285a11642771705f1ac73c09b76a
dyannelee26 <dyalee26@bergen.org>
2023-02-02 13:13:40 -0500

    imported

--- start_page.py
+++ start_page.py
@@ -1,1 +1,7 @@
-from tkinter impo
+from tkinter import *
+
+class Application(Frame):
+    def __init__(self, master):
+        super(Application, self).__init(master)
+        self.grid()
+        self.create_widgets()
