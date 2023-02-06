e70bb25e338092112838fc9999540d2b4e5dc03c
lejubi <junalee515@gmail.com>
2023-02-06 12:46:22 -0500

    main.py

--- bg_info_screen.py
+++ bg_info_screen.py
@@ -1,44 +1,36 @@
 from tkinter import *
 
 class BackgroundInfo(Frame):
-    def __init__(self, master):
+    def __init__(self, master, callback_on_bginfo):
         super(BackgroundInfo, self).__init__(master)
         self.grid()
         self.create_widgets()
+        self.callback_on_bginfo = callback_on_bginfo
+
     
     def create_widgets(self):
-        Label(root, text="Background Information", font="Georgia 40 bold", fg="black", bg="#F0F0F0").place(x=625, y=60, anchor=CENTER)
+        Label(self, text="Background Information", font="Georgia 40 bold", fg="black", bg="#F0F0F0").place(x=625, y=60, anchor=CENTER)
 
         i = 50
         bg_color = "#F0F0F0"
 
-        text1 = Label(root, text="Year 2845, the Gods have abandoned human civilization. No one knows what exactly happened.", font="Georgia 24", bg=bg_color)
+        text1 = Label(self, text="Year 2845, the Gods have abandoned human civilization. No one knows what exactly happened.", font="Georgia 24", bg=bg_color)
         text1.place(x=625, y=150, anchor=CENTER)
-        text2 = Label(root, text="Just that one day, half the population vanished, and the animals turned feral. Their eyes", font="Georgia 24", bg=bg_color)
+        text2 = Label(self, text="Just that one day, half the population vanished, and the animals turned feral. Their eyes", font="Georgia 24", bg=bg_color)
         text2.place(x=625, y=150+i, anchor=CENTER)
-        text3 = Label(root, text="turned horrifying red, and their mouths drizzled with their salvia, their wanting for blood.", font="Georgia 24", bg=bg_color)
+        text3 = Label(self, text="turned horrifying red, and their mouths drizzled with their salvia, their wanting for blood.", font="Georgia 24", bg=bg_color)
         text3.place(x=625, y=150+i*2, anchor=CENTER)
-        text4 = Label(root, text="Even the pets kept in individuals\' households turned this way, and those who smiled and", font="Georgia 24", bg=bg_color)
+        text4 = Label(self, text="Even the pets kept in individuals\' households turned this way, and those who smiled and", font="Georgia 24", bg=bg_color)
         text4.place(x=625, y=150+i*3, anchor=CENTER)
-        text5 = Label(root, text="hugged their pets were bitten. Those who were bitten, then took the form of more horrifying", font="Georgia 24", bg=bg_color)
+        text5 = Label(self, text="hugged their pets were bitten. Those who were bitten, then took the form of more horrifying", font="Georgia 24", bg=bg_color)
         text5.place(x=625, y=150+i*4, anchor=CENTER)
-        text6 = Label(root, text="creatures that can no longer be called \'human\'.", font="Georgia 24", bg=bg_color)
+        text6 = Label(self, text="creatures that can no longer be called \'human\'.", font="Georgia 24", bg=bg_color)
         text6.place(x=625, y=150+i*5, anchor=CENTER)
 
-        next_button = Button(root, text="Next", font = "Ariel 20", width=12, height=2, bg="#A0A0A0") # add the command later
+        next_button = Button(self, text="Next", font = "Ariel 20", width=12, height=2, bg="#A0A0A0") # add the command later
         next_button.place(x=525, y=600, anchor=CENTER)
-        quit_button = Button(root, text="Quit", font = "Ariel 20", command=self.quit_game, width=12, height=2)
+        quit_button = Button(self, text="Quit", font = "Ariel 20", command=self.quit_game, width=12, height=2)
         quit_button.place(x=725, y=600, anchor=CENTER)
     
     def quit_game(self):
-        root.destroy()
-        
-
-root = Tk()
-root.title("Background Information")
-root.geometry("1250x750")
-app = BackgroundInfo(root)
-bg_color = "#F0F0F0"
-root.config(bg=bg_color)
-root.resizable(width=False,height=False)
-root.mainloop()
+        self.destroy()
--- main.py
+++ main.py
@@ -17,9 +17,9 @@ class Main (object):
         # Changes the window's title
         self.root.title ("Start")
         # Creates and displays a Character Selection screen
-        self.current_screen = StartPage(master = self.root)
+        self.current_screen = StartPage(master = self.root, callback_on_start = self.onclose_start_page)
                
-    def onclose_character_selection (self, selected_char_index):
+    def onclose_start_page (self):
         # Destroys the Character Selection window
         self.current_screen.destroy()
 
@@ -33,7 +33,7 @@ class Main (object):
         self.root.title ("How to Play")
 
         # Creates and displays a Prepare To Battle screen
-        self.current_screen = How_To_Play(master = self.root, callback_on_commence_battle = self.onclose_how_to_play)
+        self.current_screen = How_To_Play(master = self.root, callback_on_htp = self.onclose_how_to_play)
     
     def onclose_how_to_play (self):
         ''' 
@@ -53,7 +53,7 @@ class Main (object):
         self.root.title ("Background Info")
 
         # Creates and displays a Battle screen
-        self.current_screen = BackgroundInfo(master= self.root,  callback_on_exit = self.onclose_bg_info_screen)
+        self.current_screen = BackgroundInfo(master= self.root,  callback_on_bginfo = self.onclose_bg_info_screen)
 
     def onclose_bg_info_screen (self):
         # Destroy the entire program's window, which includes the Battle screen.
@@ -61,11 +61,11 @@ class Main (object):
         
 def main():
     # Create the battle manager, which creates the tkinter window.
-    main = Main()
+    app = Main()
     # The program begins with the Character Selection screen!
-    main.setup_character_selection()
+    app.setup_start_page()
     # Run the program!
-    main.root.mainloop()
+    app.root.mainloop()
  
 main()
     
