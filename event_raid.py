bdcd7d5ab17f991d7e466546d4f769ef401b121c
lejubi <junalee515@gmail.com>
2023-02-07 13:13:38 -0500

    failing

--- event_raid.py
+++ event_raid.py
@@ -2,7 +2,7 @@ from tkinter import *
 class EventRaid(Frame):
     
     def __init__(self, master, callback_on_raid):
-        super().__init__(master)
+        super(EventRaid, self).__init__(master)
         self.grid()
         self.create_widgets()
         self.callback_on_raid = callback_on_raid
@@ -10,11 +10,11 @@ class EventRaid(Frame):
     def create_widgets(self):
         # self.columnconfigure(0,weight=2)
 
-        mybutton1 = Button(self, text = "Stay", font = "Times 30", width = 15, command = self.start)
+        mybutton1 = Button(self, text = "Stay", font = "Times 30", width = 15)
         mybutton1.grid(row=1,column=0)
 
-        mybutton1 = Button(self, text = "Raid", font = "Times 30", width = 15, command = self.start)
+        mybutton1 = Button(self, text = "Raid", font = "Times 30", width = 15)
         mybutton1.grid(row=1,column=4)
     
-    def start(self):
-        self.callback_on_start()
+    # def start(self):
+    #     self.callback_on_start()
