ea5a69cd11f974c16310b93e28ad0b000605c969
bomiejun <bomjun26@bergen.org>
2023-02-10 13:11:27 -0500

    progress bars done ish

--- base_screen.py
+++ base_screen.py
@@ -13,23 +13,30 @@ class BaseScreen(Frame):
         """self.x = 1"""
 
     def create_widgets(self):
-        def lower()
+        def lower_supplies():
+            bar2['value'] -= 5
+        health = ttk.Style()
+        health.theme_use('default')
+        health.configure("health.Horizontal.TProgressbar", background='#00FF2B')
         
-        Label(self, text="Health: ", font="Ariel 16").grid(row=1, column=0, sticky=W)
-        self.health = Label(self, text="100", font="Ariel 16").grid(row=1, column=1, sticky=W)
+        supplies = ttk.Style()
+        supplies.theme_use('default')
+        supplies.configure("supplies.Horizontal.TProgressbar", background='blue')
 
-        Label(self, text="Supplies: ", font="Ariel 16").grid(row=3, column=0, sticky=W)
-        self.supplies = Label(self, text="100", font="Ariel 16").grid(row=3, column=1, sticky=W)
-
-
-
-        step()
-        self.after(3000, change_dir)
+        bar = Progressbar(self, length=300, style='health.Horizontal.TProgressbar')
+        bar['value'] = 95
+        Label(self, text="Health", font="Ariel 10").grid(row=1, column=0, padx=6.5, sticky=W)
+        bar.grid(column=0, row=0, padx = 10, pady=(10, 0), sticky=S)
+        
+        bar2 = Progressbar(self, length=300, style='supplies.Horizontal.TProgressbar', mode="determinate")
+        bar2['value'] = 100
+        Label(self, text="Supplies", font="Ariel 10").grid(row=3, column=0, padx=6.5, sticky=W)
+        bar2.grid(column=0, row=2, padx = 10, sticky=W)
 
         vspacing = Label(self, text=" ",height=30)
         vspacing.grid(row=4,column=2)
 
-        stay = Button(self, text = "Stay", font = "Times 30", width = 20, height=2)
+        stay = Button(self, text = "Stay", font = "Times 30", width = 20, height=2, command=lower_supplies)
         stay.grid(row=5,column=2,sticky=E)
         # stay.place(x=300,y=300)
 
