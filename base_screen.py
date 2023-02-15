a730cee7e1761e5ffa786b98b16b096dc5e5269c
lejubi <junalee515@gmail.com>
2023-02-15 13:51:11 -0500

    combined base and battle screen

--- base_screen.py
+++ base_screen.py
@@ -1,30 +1,16 @@
 from tkinter import * 
 from tkinter import ttk
 from tkinter.ttk import Progressbar
-
-HEALTH = 100
+import random
 class BaseScreen(Frame):
-    def __init__(self, master, callback_on_base):
+    def __init__(self, master):
         super(BaseScreen, self).__init__(master)
         self.grid()
-        self.create_widgets()
-        self.callback_on_base = callback_on_base
+        self.create_base_widgets()
 
-    def create_widgets(self):
-        # bg = PhotoImage(file = "base_background.jpg")
-        
-        # # Create Canvas
-        # canvas1 = Canvas(self, width = 1250,
-        #                 height = 750)
-        
-        # canvas1.pack(fill = "both", expand = True)
-        
-        # # Display image
-        # canvas1.create_image( 0, 0, image = bg, 
-        #                     anchor = "nw")
+    def create_base_widgets(self):
         self.health=100
         self.supplies=100
-
         self.healthlb = Label(self, text="Health: " + str(self.health), font="Ariel 18")
         self.healthlb.grid(row=0, column=0, sticky=W, padx=(5, 10), pady=(5, 0))
         
@@ -32,14 +18,14 @@ class BaseScreen(Frame):
         self.supplieslb.grid(row=1, column=0, sticky=W, padx=(5, 10))
         
 
-        vspacing = Label(self, text=" ")
-        vspacing.grid(row=4,column=2)
+        self.vspacing = Label(self, text=" ")
+        self.vspacing.grid(row=4,column=2)
 
-        stay = Button(self, text = "Stay", width = 20, height=2, command=self.lower_supplies)
-        stay.grid(row=5,column=2,sticky=E)
+        self.stay = Button(self, text = "Stay", width = 20, height=2, command=self.lower_supplies)
+        self.stay.grid(row=5,column=2,sticky=E)
         
-        raid = Button(self, text = "Raid", width = 20, height=2, command=self.to_battle)
-        raid.grid(row=5,column=3)
+        self.raid = Button(self, text = "Raid", width = 20, height=2, command=self.to_battle)
+        self.raid.grid(row=5,column=3)
 
         self.update()
 
@@ -48,4 +34,51 @@ class BaseScreen(Frame):
         self.supplieslb['text'] = "Supplies: " + str(self.supplies)
 
     def to_battle(self):
-        self.callback_on_base()
+        self.destroy_base_widgets()
+    
+    def destroy_battle_widgets_nofight(self):
+        self.fighttext.destroy()
+        self.run.destroy()
+        self.fight.destroy()
+        self.create_base_widgets()
+
+    def destroy_battle_widgets_afterfight(self):
+        self.fighttext.destroy()
+        self.run.destroy()
+        self.fight.destroy()
+        self.okay.destroy()
+        self.fought.destroy()
+        self.create_base_widgets
+    
+    # BATTLE SCREEN PAGE
+
+    def destroy_base_widgets(self):
+        self.healthlb.destroy()
+        self.supplieslb.destroy()
+        self.stay.destroy()
+        self.raid.destroy()
+        self.vspacing.destroy()
+        self.create_battle_widgets()
+    
+    def create_battle_widgets(self):
+        # self.columnconfigure(0,weight=2)
+        self.fighttext = Label(self,text=f"You have a {self.health}% chance of winning. Do you want to fight or run?", font="Times 32")
+
+        self.fighttext.grid(row=0, column=0, columnspan=2, sticky=N)
+
+        print("test1")
+
+        self.run = Button(self, text = "Run", font = "Times 30", width = 15, command = self.destroy_battle_widgets_nofight)
+        self.run.grid(row=1,column=0)
+
+        self.fight = Button(self, text = "Fight", font = "Times 30", width = 15, command = self.fight)
+        self.fight.grid(row=1,column=1)
+    
+    def fight(self):
+        healthlost=random.randint(1, self.health//8)
+        self.fought=Label(self, text=f"You fought the zombie and lost {healthlost} health.")
+        self.fought.grid(row=2,column=0)
+        self.health -= healthlost
+
+        okay=Button(self,text=f"Okay", command=self.destroy_battle_widgets_afterfight)
+        okay.grid(row=3,column=0)
