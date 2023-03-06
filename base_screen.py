f89856f399a25049b8426cdea6126c78e44f2d27
bomiejun <bomjun26@bergen.org>
2023-03-06 09:28:13 -0500

    done

--- base_screen.py
+++ base_screen.py
@@ -424,6 +424,7 @@ class BaseScreen(Frame):
         self.luck_vspacing2.destroy()
         self.AM_btn.destroy()
         self.NZ_btn.destroy()
+        self.luckytext.destroy()
 
     def destroy_base_widgets_for_luck(self):
         self.destroy_base_widgets()
@@ -496,7 +497,8 @@ class BaseScreen(Frame):
         self.endluck_vspacing.grid(row=0,column=0,columnspan=2, sticky=E)
 
         self.health = 5
-        self.luckytext.config(text = f"You made your choices.\n Your health is now at 5.\n\nGood luck surviving.\nIf you can. MWAHAHAHAHA",font="Luminari 30",width=70)
+        self.luckytext = Label(self,text=f"You made your choices.\n Your health is now at 5.\n\nGood luck surviving.\nIf you can. MWAHAHAHAHA",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
 
         self.endluck_spacing2=Label(self, text="", height=5)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
@@ -511,7 +513,8 @@ class BaseScreen(Frame):
         self.endluck_vspacing.grid(row=0,column=0,sticky=E,columnspan=2)
 
         self.health = 95
-        self.luckytext.config(text = f"You made your choices.\n Your health is now at 95.\n\nYou, are very lucky.\nGood luck with survival...sigh",font="Luminari 30",width=70)
+        self.luckytext = Label(self,text=f"You made your choices.\n Your health is now at 95.\n\nYou, are very lucky.\nGood luck with survival...sigh",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
 
         self.endluck_spacing2=Label(self, text="", height=5)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
@@ -546,7 +549,9 @@ class BaseScreen(Frame):
 
         self.health = random.randint(1,10)
         self.supplies = random.randint(1,10)
-        self.luckytext.config(text = f"Why would you do that?!?\n Your health is now at {str(self.health)}.\n Your supplies are at {str(self.supplies)}\n\nHonestly, you are hopeless.\nSee you around... If you're around",font="Luminari 30",width=70)
+
+        self.luckytext = Label(self, text=f"Why would you do that?!?\n Your health is now at {str(self.health)}.\n Your supplies are at {str(self.supplies)}\n\nHonestly, you are hopeless.\nSee you around... If you're around",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
 
         self.endluck_spacing2=Label(self, text="", height=2)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
@@ -561,7 +566,8 @@ class BaseScreen(Frame):
         self.endluck_vspacing.grid(row=0,column=0,sticky=E,columnspan=2)
 
         self.supplies = random.randint(30,50)
-        self.luckytext.config(text = f"I like food too.\n Buuuuut I think you need to keep a healthy diet.\n\nYour food supply is at {str(self.supplies)}.\nWhat you eat is very important",font="Luminari 30",width=70)
+        self.luckytext = Label(self, text=f"I like food too.\n Buuuuut I think you need to keep a healthy diet.\n\nYour food supply is at {str(self.supplies)}.\nWhat you eat is very important",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
 
         self.endluck_spacing2=Label(self, text="", height=2)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
@@ -618,7 +624,8 @@ class BaseScreen(Frame):
             self.health = 1
         if self.dice == 1:
             self.health = 100
-        self.luckytext.config(text = f"I can't believe you tried to gamble your health.\n Your health is now at {str(self.health)}.\n\nYou know, it was a 1 or 100.\nI really don't know if it was worth it.",font="Luminari 30",width=70)
+        self.luckytext = Label(self, text=f"I can't believe you tried to gamble your health.\n Your health is now at {str(self.health)}.\n\nYou know, it was a 1 or 100.\nI really don't know if it was worth it.",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
 
         self.endluck_spacing2=Label(self, text="", height=2)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
@@ -637,7 +644,8 @@ class BaseScreen(Frame):
             self.supplies = 1
         if self.dice == 1:
             self.supplies = 100
-        self.luckytext.config(text = f"You gambled whaaaat?\n Your supplies?!?\n\nYour supply level is at {str(self.supplies)}.\nJust know, if you don't have any supplies, you may just...",font="Luminari 30",width=70)
+        self.luckytext = Label(self, text=f"You gambled whaaaat?\n Your supplies?!?\n\nYour supply level is at {str(self.supplies)}.\nJust know, if you don't have any supplies, you may just...",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
 
         self.endluck_spacing2=Label(self, text="", height=2)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
@@ -670,7 +678,9 @@ class BaseScreen(Frame):
         self.endluck_vspacing.grid(row=0,column=0,sticky=E,columnspan=2)
 
         self.health = 75
-        self.luckytext.config(text = f"Wow...\n You cheated, didn't you\n Your health is now at 75.\n\nJust know I'm not encouraging cheating.\nThere just is that tiny possibility you actually knew...(wink)",font="Luminari 30",width=70)
+
+        self.luckytext = Label(self, text=f"Wow...\n You cheated, didn't you\n Your health is now at 75.\n\nJust know I'm not encouraging cheating.\nThere just is that tiny possibility you actually knew...(wink)",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
 
         self.endluck_spacing2=Label(self, text="", height=2)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
@@ -686,8 +696,9 @@ class BaseScreen(Frame):
 
         self.health = 100
         self.supplies = 100
-        self.luckytext.config(text = f"You didn't know...\nWell, cows can grow up to 6ft 4in.\nBut since you were sincere,\n\nYour health is at {str(self.health)}.\nYour supply is at {str(self.supplies)}.",font="Luminari 30",width=70)
-
+        self.luckytext = Label(self, text=f"You didn't know...\nWell, cows can grow up to 6ft 4in.\nBut since you were sincere,\n\nYour health is at {str(self.health)}.\nYour supply is at {str(self.supplies)}.",font="Luminari 30",width=70)
+        self.luckytext.grid(row=1, column=0, columnspan=2, sticky=N)
+        
         self.endluck_spacing2=Label(self, text="", height=2)
         self.endluck_spacing2.grid(row=2, column=0, sticky=E,columnspan=2)
 
